b4bf25ff4e2589b53e441db646a82c76ec0d6ae4
EmilSunden <emilsunjobb@gmail.com>
2023-09-28 14:05:58 +0200

    feat: Adds customer route

---
+++ app/kundservice/page.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+        <h1>Kundservice sida</h1>
+        <Link href="/">Hem</Link>
+    </div>
+  )
+}
