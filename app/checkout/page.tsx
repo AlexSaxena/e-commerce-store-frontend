b5a69eabce32954b90020c084acca98ff7f5fe63
EmilSunden <emilsunjobb@gmail.com>
2023-09-28 14:04:57 +0200

    feat: Adds checkout route

---
+++ app/checkout/page.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+        <h1>Checkout page</h1>
+        <Link href="/">Hem</Link>
+    </div>
+  )
+}
