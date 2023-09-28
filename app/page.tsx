1ef17b73ba3bf1f76a4903c64c3f92bd61b6e46f
EmilSunden <emilsunjobb@gmail.com>
2023-09-28 14:32:24 +0200

    feat: Deletes nav tag, Adds Footer component

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
-import Image from 'next/image'
-import Link from 'next/link'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 export default function Home() {
   return (
     <main>
-      <nav>
-        <h1>Home</h1>
           <Navbar />
-      </nav>
+          <Footer />
     </main>
   )
 }
