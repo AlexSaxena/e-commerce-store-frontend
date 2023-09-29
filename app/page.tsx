29440aa4411a6dc9c91bc255634b4eb49793e0d4
KennethEllebring <kenneth.ellebring@yh.nackademin.se>
2023-09-29 15:56:52 +0200

    feat: mapping function for home

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+import prisma from "./utils/connectdb";
+// import Navbar from './components/Navbar'
+// import Footer from './components/Footer'
 
-export default function Home() {
+export default async function Home() {
+  const products = await prisma.product.findMany();
   return (
     <main>
-        <h1>Hem</h1>
+      <h1>Hem</h1>
+      <h2 className="font-bold">Products</h2>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>{product.name}</li>
+        ))}
+      </ul>
     </main>
-  )
+  );
 }
