957981fa14f0bf38ca7358a86e15d06a74212a0f
AlexSaxena <saxenaalex@gmail.com>
2023-10-02 14:28:20 +0200

    feat: adds base of login & register

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,8 @@
+export default function LoginPage() {
+    return (
+      <div>
+        <h1 style={{color:"salmon"}}>Login Page</h1>
+      </div>
+    );
+  }
+  
---
+++ app/auth/register/page.tsx
@@ -0,0 +1,8 @@
+export default function RegisterPage() {
+    return (
+      <div>
+        <h1 style={{color:"salmon"}}>Register Page</h1>
+      </div>
+    );
+  }
+  
