aa97d54800288905c212d495db647aa96376b420
AlexSaxena <saxenaalex@gmail.com>
2023-10-02 17:04:21 +0200

    feat: adds state to register form for all inputs

--- app/auth/register/form.tsx
+++ app/auth/register/form.tsx
@@ -1,27 +1,58 @@
-'use-client';
+'use client';
+import { useState } from 'react';
+
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 
 export const RegisterForms = () => {
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
   const onSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    console.log('Register!');
+
+    // API REQUEST HERE
+
+    console.log(
+      'Email: ',
+      email,
+      '\nUsername: ',
+      username,
+      '\nPassword: ',
+      password,
+    );
   };
 
   return (
-    <form className="space-y-12 w-[400px]">
+    <form onSubmit={onSubmit} className="space-y-12 w-[400px]">
       <div className="grid w-full max-w-sm items-center gap-1.5">
-        <Label htmlFor="mail">Email</Label>
-        <Input id="mail" type="mail" />
+        <Label htmlFor="email">Email</Label>
+        <Input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          id="email"
+          type="email"
+        />
       </div>
       <div className="grid w-full max-w-sm items-center gap-1.5">
         <Label htmlFor="username">Användarnamn</Label>
-        <Input id="username" type="text" />
+        <Input
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          id="username"
+          type="text"
+        />
       </div>
       <div className="grid w-full max-w-sm items-center gap-1.5">
         <Label htmlFor="password">Lösenord</Label>
-        <Input id="password" type="password" />
+        <Input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          id="password"
+          type="password"
+        />
       </div>
       <div className="w-full">
         <Button className="w-full bg-purple-800" size="lg">
