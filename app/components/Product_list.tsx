f10882c6f0dcf12f2549ce931ff6515787c211f6
KennethEllebring <kenneth.ellebring@yh.nackademin.se>
2023-10-02 23:11:43 +0200

    fix: added weight to list and config img path

--- app/components/Product_list.tsx
+++ app/components/Product_list.tsx
@@ -7,20 +7,21 @@ export default async function Product_list() {
     const products = await prisma.product.findMany();
 
   return (
-        <div className="flex flex-wrap justify-center text-center">
+        <div className="flex flex-wrap justify-center gap-8 text-center w-5/6 ">
             {products.map((product) => (
-                <div key={product.id} className="flex basis-1/4 flex-col items-center hover:border-purple-800 border-solid border-2 rounded">     
-                    <Link href="/" className="">
+                <div key={product.id} className="flex basis-1/5 flex-col items-center hover:border-purple-800 border-solid border-2 rounded">     
+                    <Link href="/#" className="flex flex-col items-center">
                         <Image 
                             src={product.image}
-                            width={320}
-                            height={320}
+                            width={208}
+                            height={208}
                             alt={product.description}
-                            className="mt-6 mb-4 h-min-300"    
+                            className="mt-6 mb-4 min-h-208 hover:scale-110 transition duration-500 cursor-pointer"    
                         />
-                        <h2 className="text-black dark:text-slate-500 text-2xl">{product.name}</h2>
-                        <p className=" w-80 h-16 text-black dark:text-slate-500">{product.description}</p>
-                        <p className="text-black dark:text-slate-500 text-3xl">{product.price.toNumber()} kr</p>
+                        <h2 className="text-xl h-16 text-black">{product.name}</h2>
+                        <p className="text-2xl h-8 text-red-600 font-black">{product.price.toNumber()} kr</p>
+                        <small className="text-slate-400">{product.weight}g</small>
+
                         </Link>
                         <button className="mt-4 mb-4 align-bottom w-28 bg-transparent hover:bg-purple-800 text-purple-800 font-semibold hover:text-white py-2 px-4 border border-purple-500 hover:border-transparent rounded">Köp</button>
                     </div>                
