9fac1127430c56053aa217330b122f67fa33c022
AlexSaxena <saxenaalex@gmail.com>
2023-10-02 15:52:20 +0200

    style: imports comp & adds tailwindstyling

--- app/auth/register/form.tsx
+++ app/auth/register/form.tsx
@@ -1,4 +1,7 @@
 'use-client';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 
 export const RegisterForms = () => {
   const onSubmit = (e: React.FormEvent) => {
@@ -7,10 +10,18 @@ export const RegisterForms = () => {
   };
 
   return (
-    <form>
-      <input />
-      <input />
-      <button>Register</button>
+    <form className="space-y-12 w-[400px]">
+      <div className="grid w-full max-w-sm items-center gap-1.5">
+        <Label htmlFor="username">Username</Label>
+        <Input id="username" type="text" />
+      </div>
+      <div className="grid w-full max-w-sm items-center gap-1.5">
+        <Label htmlFor="password">Password</Label>
+        <Input id="password" type="password" />
+      </div>
+      <div className="w-full">
+        <Button className="">Register</Button>
+      </div>
     </form>
   );
 };
--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -4,8 +4,8 @@ import { RegisterForms } from './form';
 export default function RegisterPage() {
   return (
     <div className="h-screen w-screen flex justify-center items-center bg-slate-100">
-      <div className="shadow-xl p-4 bg-white rounded-xl">
-        <h1>Register Page</h1>
+      <div className="shadow-xl px-8 py-8 pt-12 bg-white rounded-xl space-y-12">
+        <h1 className="font-semibold text-2xl">Bli En Medlem!</h1>
         <RegisterForms />
       </div>
     </div>
