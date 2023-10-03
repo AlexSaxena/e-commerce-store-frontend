2ea6e5ef41096730ef2d390c52c267b0adede8a1
EmilSunden <emilsunjobb@gmail.com>
2023-10-03 10:34:37 +0200

    feat: adds login form

---
+++ app/auth/login/form.tsx
@@ -0,0 +1,69 @@
+'use client';
+import { useState } from 'react';
+
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+
+export const RegisterForms = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    // API REQUEST HERE
+    try {
+      const res = await fetch('hakim/product/db', {
+        method: 'post',
+        body: JSON.stringify({
+          username,
+          password,
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      if (res.ok) {
+        // Redirect or something
+      }
+    } catch (error) {
+      console.error(error);
+    }
+
+    console.log(
+      'Username: ',
+      username,
+      '\nPassword: ',
+      password,
+    );
+  };
+
+  return (
+    <form onSubmit={onSubmit} className="space-y-12 w-[400px]">
+      <div className="grid w-full max-w-sm items-center gap-1.5">
+        <Label htmlFor="username">Användarnamn</Label>
+        <Input
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          id="username"
+          type="text"
+        />
+      </div>
+      <div className="grid w-full max-w-sm items-center gap-1.5">
+        <Label htmlFor="password">Lösenord</Label>
+        <Input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          id="password"
+          type="password"
+        />
+      </div>
+      <div className="w-full">
+        <Button className="w-full bg-purple-800" size="lg">
+          Registrera
+        </Button>
+      </div>
+    </form>
+  );
+};
