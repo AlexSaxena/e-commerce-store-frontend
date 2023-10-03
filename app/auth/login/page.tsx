0f1ae37ce8542c3c23b6a41b88d205275aaa559b
EmilSunden <emilsunjobb@gmail.com>
2023-10-03 10:35:20 +0200

    feat: adds content including form component to page

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,8 +1,21 @@
+import { RegisterForms } from './form';
+import Link from 'next/link';
+
 export default function LoginPage() {
     return (
-      <div>
-        <h1 style={{color:"salmon"}}>Login Page</h1>
+      <div className="h-screen w-screen flex justify-center items-center bg-slate-100">
+      <div className="shadow-xl px-8 py-8 pt-12 bg-white rounded-xl space-y-12">
+        <h1 className="font-semibold text-2xl">Bli En Medlem!</h1>
+        <RegisterForms />
+        <p className="text-center">
+          Inte Medlem?
+          <Link className="text-indigo-500 hover:underline" href="/auth/register">
+            {' '}
+            Registrera dig!
+          </Link>
+        </p>
       </div>
+    </div>
     );
   }
   
