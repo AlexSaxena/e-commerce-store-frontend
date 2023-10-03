e4fd21c77807adb9b9358eac2c635da8c4ab4eda
EmilSunden <emilsunjobb@gmail.com>
2023-10-03 11:51:29 +0200

    feat: adds styling to navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,19 +3,19 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <nav className='bg-black'>
-        <p className='text-xl '>Navbar</p>
-        
-        <h1>Hakims Livs</h1>
-        <div>
-            <Link href="/om-hakim-livs">Om Hakims Livs</Link>
-                <input 
-                className='searchbar bg-white'
-                type="text" 
-                placeholder='search'
-                />
-            <Link href="/checkout">Varukorg</Link>
-        </div> 
+    <nav className='bg-black p-12'>
+        <div className='grid grid-cols-2'>
+          <h1 className='text-5xl text-center col-span-1 col-start-1'>Hakims Livs</h1>
+            <div className='border border-red-200 col-span-1 grid grid-cols-2 items-center col-start-3'>
+                {/* <Link href="/om-hakim-livs">Om Hakims Livs</Link> */}
+                    <input 
+                    className='searchbar bg-white rounded-full h-8 border-none w-96 text-center'
+                    type="text" 
+                    placeholder='search'
+                    />
+                <Link href="/checkout" className='text-center'>Varukorg</Link>
+            </div> 
+        </div>
     </nav>
   )
 }
