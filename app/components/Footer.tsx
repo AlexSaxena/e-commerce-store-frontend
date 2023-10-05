6faf0e67931cd9b3e78402917fefddf7144f9b78
AlexSaxena <saxenaalex@gmail.com>
2023-10-05 18:49:13 +0200

    fix: updates link-title in footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -23,17 +23,17 @@ export default function Footer() {
           <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
             <li>
               <Link href="#" className="mr-4 hover:underline md:mr-6 ">
-                Om Oss
+                Hem
               </Link>
             </li>
             <li>
               <Link href="#" className="mr-4 hover:underline md:mr-6">
-                Vår Vision
+                Om Oss
               </Link>
             </li>
             <li>
               <Link href="#" className="mr-4 hover:underline md:mr-6 ">
-                Kategorier
+                Vår Vision
               </Link>
             </li>
             <li>
