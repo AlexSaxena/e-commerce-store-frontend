6df40b37c0e67b5d5779c84b7a1f7c0073e85303
AlexSaxena <saxenaalex@gmail.com>
2023-10-05 18:44:41 +0200

    fix: makes box-shadow slightly stronger

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,7 +5,7 @@ import logo from '../../public/assets/logos/logo-hakim-01.png';
 
 export default function Footer() {
   return (
-    <footer className="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
+    <footer className="bg-white rounded-lg shadow-md dark:bg-gray-900 m-4">
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
           <Link href="/" className="flex items-center mb-4 sm:mb-0">
