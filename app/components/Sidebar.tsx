9ff5a92407a4bb6e50b97daffca62f1380862063
Noel Perland <Noel.Perland@yh.nackademin.se>
2023-10-05 17:07:57 +0200

    feat: started to style the sidebar. Soon done :D

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -1,21 +1,28 @@
-import Link from 'next/link'
-import React from 'react'
-import prisma from "../utils/connectdb";
+import Link from 'next/link';
+import React from 'react';
+import prisma from '../utils/connectdb';
 
-export default async function Sidebar(){
+export default async function Sidebar() {
+  const categorys = await prisma.category.findMany();
 
-    const categorys = await prisma.category.findMany();
+  return (
+    <aside
+      role="Sidebar"
+      className="	flex basis-full text-slate-800 text-2xl font-normal "
+    >
+      <ul className=" flex flex-col space-y-8 w-full">
+        <Link href="/#" className="hover:bg-slate-200 w-full">
+          <li className="pl-4 mt-4">Erbjudanden</li>
+        </Link>
 
-    return(
-        <aside role='Sidebar' style={{
-            border: 'solid 1px blue'
-          }}>
-            <ul>
-                <li><Link href='/#' className=''>Erbjudanden</Link></li>
-                {categorys.map((category) => (
-                    <li key={category.id}><Link href="/#" className=''>{category.name}</Link></li>
-                ))}
-            </ul>
-        </aside>
-    )
+        {categorys.map((category) => (
+          <Link href="/#" className="hover:bg-slate-200 w-full ">
+            <li key={category.id} className="hover:bg-slate-200 w-full pl-4">
+              {category.name}
+            </li>
+          </Link>
+        ))}
+      </ul>
+    </aside>
+  );
 }
