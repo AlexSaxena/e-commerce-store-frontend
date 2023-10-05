775814cf02117138b815fed21afe6bd98d4be500
KennethEllebring <kenneth.ellebring@yh.nackademin.se>
2023-10-05 14:35:27 +0200

    fix: fixed wonky product list

--- app/components/Product_list.tsx
+++ app/components/Product_list.tsx
@@ -6,25 +6,25 @@ export default async function Product_list() {
   const products = await prisma.product.findMany();
 
   return (
-    <div className="flex flex-wrap justify-center gap-8 text-center w-5/6 ">
+    <div className="flex flex-wrap justify-center gap-8 text-center w-5/6">
       {products.map((product) => (
         <div
           key={product.id}
           className="flex basis-1/5 flex-col items-center hover:border-purple-800 border-solid border-2 rounded"
         >
-          <Link href="/#" className="flex flex-col items-center">
+          <Link href="/#" className="flex flex-col items-center w-72">
             <Image
               src={product.imageUrl}
               width={208}
               height={208}
               alt={product.description}
-              className="mt-6 mb-4 min-h-208 hover:scale-110 transition duration-500 cursor-pointer"
+              className="mt-6 mb-4 hover:scale-110 transition duration-500 cursor-pointer"
             />
-            <h2 className="text-xl h-16 text-black">{product.name}</h2>
-            <p className="text-2xl h-8 text-red-600 font-black">
+            <h2 className="text-xl h-16 text-black mx-4 ">{product.name}</h2>
+            <p className="text-2xl h-8 text-red-600 font-black min-w-fit">
               {product.price?.toNumber()} kr
             </p>
-            <small className="text-slate-400">{product.weight}</small>
+            <small className="text-slate-400">{product.weight}g</small>
           </Link>
           <button className="mt-4 mb-4 align-bottom w-28 bg-transparent hover:bg-purple-800 text-purple-800 font-semibold hover:text-white py-2 px-4 border border-purple-500 hover:border-transparent rounded">
             Köp
