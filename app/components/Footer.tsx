cdde0cb904838bc7ca198129099be9dc26fdd7fb
AlexSaxena <saxenaalex@gmail.com>
2023-10-05 17:36:48 +0200

    feat: imports Image & Logo

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,12 +1,13 @@
-import Link from 'next/link'
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import logo from '../../public/assets/logos/logo-hakim-01.png';
 
 export default function Footer() {
   return (
-    <footer
-    className='bg-black'>
-        <h1>Footer</h1>
-        <Link href="/kundservice">Kundservice</Link>
+    <footer className="bg-black">
+      <h1>Footer</h1>
+      <Link href="/kundservice">Kundservice</Link>
     </footer>
-  )
+  );
 }
