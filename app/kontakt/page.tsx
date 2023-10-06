1c73f87cfdd0edc8521b0d79d966e411192ad51f
EmilSunden <emilsunjobb@gmail.com>
2023-10-06 10:48:32 +0200

    feat: adds new route for contact

---
+++ app/kontakt/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='w-full flex justify-center'>
+        <form action="" className='flex flex-col items-center border max-w-fit h-fit'>
+
+        <label htmlFor="">Mejl</label>
+        <input type="text" className='border'/>
+
+        <label htmlFor="">Meddelande</label>
+        <textarea name="" id="" className='border'></textarea>
+
+        </form>
+    </div>
+  )
+}
