5d1937b852124abdf8fcd171b2d3073e828f05d0
AlexSaxena <saxenaalex@gmail.com>
2023-10-06 14:44:32 +0200

    feat: adds mail and adress to bottom of form

--- app/kontakt/page.tsx
+++ app/kontakt/page.tsx
@@ -68,6 +68,14 @@ export default function page() {
                   Skicka
                 </button>
               </div>
+              <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
+                <a className="text-indigo-500">hakimLivs@mail.com</a>
+                <p className="leading-normal my-5">
+                  fejkgatan 12
+                  <br />
+                  Stockholm, 420 1337
+                </p>
+              </div>
             </div>
           </div>
         </div>
