80b5a579efa8b37d1d782d56680721fc7b30e381
AlexSaxena <saxenaalex@gmail.com>
2023-10-06 14:35:23 +0200

    feat: adds base structure for contact form

--- app/kontakt/page.tsx
+++ app/kontakt/page.tsx
@@ -1,17 +1,44 @@
-import React from 'react'
+import React from 'react';
 
 export default function page() {
   return (
-    <div className='w-full flex justify-center'>
-        <form action="" className='flex flex-col items-center border max-w-fit h-fit'>
-
-        <label htmlFor="">Mejl</label>
-        <input type="text" className='border'/>
-
-        <label htmlFor="">Meddelande</label>
-        <textarea name="" id="" className='border'></textarea>
-
-        </form>
+    <div>
+      <section>
+        <div>
+          <div>
+            <h1>Contact Us</h1>
+            <p>
+              Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
+              gentrify.
+            </p>
+          </div>
+          <div>
+            <div>
+              <div>
+                <div>
+                  <label>Name</label>
+                  <input />
+                </div>
+              </div>
+              <div>
+                <div>
+                  <label>Email</label>
+                  <input />
+                </div>
+              </div>
+              <div>
+                <div>
+                  <label>Message</label>
+                  <textarea></textarea>
+                </div>
+              </div>
+              <div>
+                <button>Send btn</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
-  )
+  );
 }
