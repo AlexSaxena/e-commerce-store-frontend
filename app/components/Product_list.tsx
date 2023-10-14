d8f69ff8401cf73c2130b2d022c2f7e0db55f9ef
EmilSunden <emilsunjobb@gmail.com>
2023-10-14 18:32:55 +0200

    feat: imported modal

--- app/components/Product_list.tsx
+++ app/components/Product_list.tsx
@@ -1,8 +1,10 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import prisma from '../utils/connectdb';
+import Modal from './productmodal'; // Make sure to import the correct Modal component
 
 export default async function Product_list() {
+  
   const products = await prisma.product.findMany();
 
   return (
@@ -12,7 +14,7 @@ export default async function Product_list() {
           key={product.id}
           className="flex basis-1/5 flex-col items-center hover:border-purple-800 border-solid border-2 rounded"
         >
-          <Link href="/#" className="flex flex-col items-center w-72">
+          <div className="flex flex-col items-center w-72">
             <Image
               src={product.imageUrl}
               width={208}
@@ -20,17 +22,22 @@ export default async function Product_list() {
               alt={product.description}
               className="mt-6 mb-4 hover:scale-110 transition duration-500 cursor-pointer"
             />
+             
+
             <h2 className="text-xl h-16 text-black mx-4 ">{product.name}</h2>
+            
             <p className="text-2xl h-8 text-red-600 font-black min-w-fit">
               {product.price?.toNumber()} kr
             </p>
-            <small className="text-slate-400">{product.weight}g</small>
-          </Link>
+            <small className="text-slate-400">{product.weight.toString()}g</small>
+          </div>
           <button className="mt-4 mb-4 align-bottom w-28 bg-transparent hover:bg-purple-800 text-purple-800 font-semibold hover:text-white py-2 px-4 border border-purple-500 hover:border-transparent rounded">
-            Köp
+            {/* Pass imageUrl and description as props to Modal */}
+            <Modal product={product}/>
           </button>
         </div>
       ))}
     </div>
   );
 }
+
