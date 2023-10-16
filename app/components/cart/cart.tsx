5b4aa6fe1f849862e0e41a2cd0e23b867a027121
EmilSunden <emilsunjobb@gmail.com>
2023-10-16 21:51:52 +0200

    feat: created cart component

---
+++ app/components/cart/cart.tsx
@@ -0,0 +1,32 @@
+'use client';
+import { useCartStore } from '../store/cartStore';
+
+export default function Cart() {
+  // Get the cart status using the hook useCartStore, which gives us access to the cart status of the store.
+  const cart = useCartStore((state) => state.cart);
+
+  // Calculate the total price of the products in the cart by adding the prices of each product multiplied by its quantity.
+  const total = cart.reduce(
+    (acc, product) => acc + Number(product.price) * Number(product.quantity),
+    0,
+  );
+
+  return (
+    <section>
+      <ul>
+        {cart &&
+          cart.map((product) => (
+            //  <CartItem key={product.id} product={product} />
+            <div key={product.id} className="flex">
+              <img src={product.imageUrl} className="h-12 w-12" />
+              <p>{product.name}</p>
+            </div>
+          ))}
+      </ul>
+      <div className="flex justify-between items-center mt-4">
+        <span className="text-lg font-bold">Total:</span>
+        <span className="text-xl font-bold">${total.toFixed(2)}</span>
+      </div>
+    </section>
+  );
+}
