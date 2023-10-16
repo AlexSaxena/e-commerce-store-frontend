1379afed40d270f85d3023d62f4c3240bbdef192
EmilSunden <emilsunjobb@gmail.com>
2023-10-16 22:06:53 +0200

    feat: added remove function

--- app/components/cart/cart.tsx
+++ app/components/cart/cart.tsx
@@ -4,6 +4,7 @@ import { useCartStore } from '../store/cartStore';
 export default function Cart() {
   // Get the cart status using the hook useCartStore, which gives us access to the cart status of the store.
   const cart = useCartStore((state) => state.cart);
+  const removeFromCart = useCartStore((state) => state.removeFromCart)
 
   // Calculate the total price of the products in the cart by adding the prices of each product multiplied by its quantity.
   const total = cart.reduce(
@@ -17,9 +18,14 @@ export default function Cart() {
         {cart &&
           cart.map((product) => (
             //  <CartItem key={product.id} product={product} />
-            <div key={product.id} className="flex">
+            <div key={product.id} className="flex justify-between w-96">
+              <div>
               <img src={product.imageUrl} className="h-12 w-12" />
               <p>{product.name}</p>
+              </div>
+              <div>
+                <button onClick={() => removeFromCart(product)}>Remove From Cart</button>
+              </div>
             </div>
           ))}
       </ul>
