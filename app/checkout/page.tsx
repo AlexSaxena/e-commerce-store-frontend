83cdc71219c8159863e92a40c91c2df880044dc9
EmilSunden <emilsunjobb@gmail.com>
2023-10-16 13:52:33 +0200

    feat: maps through the cart with the help of state

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,11 +1,21 @@
+"use client";
 import Link from 'next/link'
 import React from 'react'
+import { useCart } from '../context/CartContext'
 
 export default function page() {
+  const {cartState} = useCart();
   return (
     <div>
         <h1>Checkout page</h1>
         <Link href="/">Hem</Link>
+        {cartState.cart.map((product) => (
+          <div key={product.id}>
+            <img src={product.imageUrl} alt="image" className='w-12 h-12'/>
+            <p>{product.name}</p>
+          </div>
+        ))}
+        <button className='border border-purple-600'>Beställ</button>
     </div>
   )
 }
