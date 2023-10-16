cd31bcf92ec2319c2adfc9ddc154e04a04f24ed3
EmilSunden <emilsunjobb@gmail.com>
2023-10-16 21:53:33 +0200

    feat: deleted provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,9 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-
-
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Sidebar from './components/Sidebar';
-
-import { CartProvider } from './context/CartContext';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -22,7 +18,6 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <CartProvider>
       <html lang="en">
         <body className={inter.className}>
           <Navbar />
@@ -34,6 +29,5 @@ export default function RootLayout({
           <Footer />
         </body>
       </html>
-    </CartProvider>
   );
 }
