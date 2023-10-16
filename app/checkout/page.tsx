eeaa27b71a1fa9e05ce7c8edc59256410e3cac8f
EmilSunden <emilsunjobb@gmail.com>
2023-10-16 22:26:28 +0200

    feat: styling

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,14 +1,14 @@
-import Link from 'next/link'
-import React from 'react'
-import Cart from '../components/cart/cart'
+import Link from 'next/link';
+import React from 'react';
+import Cart from '../components/cart/cart';
 
 export default function page() {
   return (
-    <div>
-        <h1>Checkout page</h1>
-        <Link href="/">Hem</Link>
+    <div className="flex flex-col relative left-80">
+      <div>
         <Cart />
-        <button className='border border-purple-600'>Beställ</button>
+        <button className="border border-purple-600">Beställ</button>
+      </div>
     </div>
-  )
+  );
 }
