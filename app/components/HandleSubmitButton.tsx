6bba4509027b979f0b6f380e4a02a3d6eb71bd0c
EmilSunden <emilsunjobb@gmail.com>
2023-10-17 18:40:40 +0200

    feat: added some data

--- app/components/HandleSubmitButton.tsx
+++ app/components/HandleSubmitButton.tsx
@@ -3,7 +3,9 @@ import { Product } from '@prisma/client';
 import { useCartStore } from './store/cartStore';
 import React from 'react';
 
-let storeId = '4d1875a7-1a5a-42d1-a9c1-ffa1b78bba20'; //Temporary Store ID
+// let storeId = '4d1875a7-1a5a-42d1-a9c1-ffa1b78bba20'; //Temporary Store ID
+
+let emilstore = '1b9f737b-8ed7-4b20-b3e7-d45dcd91eebe'
 
 interface HandleSubmitButtonProps {
   product: Product;
@@ -25,15 +27,34 @@ const HandleSubmitButton: React.FC<HandleSubmitButtonProps> = ({ product }) => {
 
     console.table(myOrder);
 
+    const data = {
+      storeId: emilstore,
+      orderItems: [
+        {
+          productId: '0223cee8-8884-4c6f-b093-340df860f80a',
+          quantity: 2,
+        },
+        {
+          productId: '138f3d69-5194-4846-8976-eb40171a6236',
+          quantity: 3,
+        },
+      ],
+      isPaid: true,
+      name: 'John Doe',
+      phone: '123-456-7890',
+      address: '123 Main St',
+    };
+
     try {
       const response = await fetch(
-        `http://localhost:3000/api/${storeId}/orders/addorder`, // CHECK LOCALHOST NR
+        `http://localhost:3000/api/${emilstore}/orders/addorder`, // CHECK LOCALHOST NR
         {
+          mode: 'no-cors',
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
           },
-          body: JSON.stringify(cart),
+          body: JSON.stringify(data),
         },
       );
 
