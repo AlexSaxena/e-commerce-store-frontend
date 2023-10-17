cd004e3974e70fe07b0c89a927509faa88451b4f
EmilSunden <emilsunjobb@gmail.com>
2023-10-17 12:14:29 +0200

    feat: imported addtocart component

--- app/components/Product_list.tsx
+++ app/components/Product_list.tsx
@@ -1,9 +1,9 @@
 import Image from 'next/image';
 import prisma from '../utils/connectdb';
 import Modal from './productmodal'; // Make sure to import the correct Modal component
+import AddToCart from './addToCart';
 
 export default async function Product_list() {
-  
   const products = await prisma.product.findMany();
 
   return (
@@ -21,26 +21,26 @@ export default async function Product_list() {
               alt={product.description}
               className="mt-6 mb-4 hover:scale-110 transition duration-500 cursor-pointer"
             />
-             
 
             <h2 className="text-xl h-16 text-black mx-4 ">{product.name}</h2>
-            
+
             <p className="text-2xl h-8 text-red-600 font-black min-w-fit">
               {product.price?.toNumber()} kr
             </p>
             <small className="text-slate-400">
               {product.weight.toString()}g
             </small>
-          
-            <button className="mt-4 mb-4 align-bottom w-28 bg-transparent hover:bg-purple-800 text-purple-800 font-semibold hover:text-white py-2 px-4 border border-purple-500 hover:border-transparent rounded">
-              {/* Pass imageUrl and description as props to Modal */}
-              <Modal product={product}/>
-            </button>
+
+            <div className="flex justify-between w-52">
+              <button className="mt-4 mb-4 align-bottom w-28 bg-transparent hover:bg-purple-800 text-purple-800 font-semibold hover:text-white py-2 px-4 border border-purple-500 hover:border-transparent rounded">
+                <Modal product={product} />
+              </button>
+
+              <AddToCart product={product} />
+            </div>
           </div>
         </div>
       ))}
     </div>
   );
 }
-
-
