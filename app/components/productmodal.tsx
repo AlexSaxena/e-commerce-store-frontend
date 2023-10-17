461565fe68818513a861c539bad77c7871b3a193
AlexSaxena <saxenaalex@gmail.com>
2023-10-17 22:29:19 +0200

    fix: adds toast instead of window.alert on success

--- app/components/productmodal.tsx
+++ app/components/productmodal.tsx
@@ -1,23 +1,24 @@
 // src/Modal.server.js
-"use client";
+'use client';
 import { useState } from 'react';
 import { Product } from '@prisma/client';
 import { useCartStore } from './store/cartStore';
+import { toast } from 'react-toastify';
 
 interface ModalProps {
   product: Product;
 }
 
 export default function Modal({ product }: ModalProps) {
-  const addToCart = useCartStore(state => state.addToCart)
+  const addToCart = useCartStore((state) => state.addToCart);
 
   const handleClick = () => {
     addToCart(product);
-    window.alert('Product added to cart')
-  }
- 
+    toast.success('Product added to cart');
+  };
+
   const [isOpen, setIsOpen] = useState(false);
-  const [isDescOpen, setIsDescOpen] = useState(false)
+  const [isDescOpen, setIsDescOpen] = useState(false);
 
   const openModal = () => {
     setIsOpen(true);
@@ -32,15 +33,16 @@ export default function Modal({ product }: ModalProps) {
   };
 
   const toggleDescription = () => {
-    setIsDescOpen(!isDescOpen)
-  }
+    setIsDescOpen(!isDescOpen);
+  };
 
   return (
     <>
       <button onClick={openModal}>Mer Info</button>
       {isOpen && (
         <div className="fixed inset-0 flex items-center justify-center z-50">
-          <div className="absolute inset-0 bg-black opacity-50"></div> {/* Backdrop overlay */}
+          <div className="absolute inset-0 bg-black opacity-50"></div>{' '}
+          {/* Backdrop overlay */}
           <div className="bg-white p-6 rounded-lg shadow-lg w-1/3 h-2/4 relative z-10 text-black">
             <button onClick={closeModal} className="absolute top-2 right-2">
               X
@@ -51,28 +53,26 @@ export default function Modal({ product }: ModalProps) {
               </div>
               <div className="w-1/2 p-4 flex items-center flex-col">
                 <h3 className="text-xl font-semibold">{product.name}</h3>
-                  <div className='flex flex-col'>
-                    <p>{product.weight.toString()} ml</p>
-                    <p>{product.price.toString()} kr</p>
-                  </div>
-                <button className="bg-purple-500 text-white px-4 py-2 rounded-full mt-32 w-3/4"
-                onClick={handleClick}
+                <div className="flex flex-col">
+                  <p>{product.weight.toString()} ml</p>
+                  <p>{product.price.toString()} kr</p>
+                </div>
+                <button
+                  className="bg-purple-500 text-white px-4 py-2 rounded-full mt-32 w-3/4"
+                  onClick={handleClick}
                 >
                   Lägg till i varukorg
                 </button>
               </div>
             </div>
-            <div className='mt-4'>
-              <button onClick={toggleDescription}
-              className="text-blue-500 hover:underline"
+            <div className="mt-4">
+              <button
+                onClick={toggleDescription}
+                className="text-blue-500 hover:underline"
               >
                 {isDescOpen ? 'Hide Description' : 'Show Description'}
               </button>
-              {isDescOpen && (
-                <p className='mt-4'>
-                  Description
-                </p>
-              )}
+              {isDescOpen && <p className="mt-4">Description</p>}
             </div>
           </div>
         </div>
