8dad5ed5498d98b49ba523a7b89f4291c10d0d70
AlexSaxena <saxenaalex@gmail.com>
2023-10-17 21:30:27 +0200

    feat: adds react-toast for adding product 2 cart

--- app/components/addToCart.tsx
+++ app/components/addToCart.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { Product } from '@prisma/client';
 import { useCartStore } from './store/cartStore';
+import { toast } from 'react-toastify';
 
 interface ModalProps {
   product: Product;
@@ -11,15 +12,17 @@ export default function AddToCart({ product }: ModalProps) {
 
   const handleClick = () => {
     addToCart(product);
-    window.alert('Product added to cart')
-  }
+    toast.success('Product added to cart');
+  };
 
   return (
     <>
-      <button 
-      onClick={handleClick}
-      className='mt-4 mb-4 align-bottom w-28 bg-transparent hover:bg-purple-800 text-purple-800 font-semibold hover:text-white py-2 px-4 border border-purple-500 hover:border-transparent rounded'
-      >Köp</button>
+      <button
+        onClick={handleClick}
+        className="mt-4 mb-4 align-bottom w-28 bg-transparent hover:bg-purple-800 text-purple-800 font-semibold hover:text-white py-2 px-4 border border-purple-500 hover:border-transparent rounded"
+      >
+        Köp
+      </button>
     </>
   );
 }
