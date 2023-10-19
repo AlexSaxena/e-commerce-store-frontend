af3f814d2c55ffeca5682c2696e52ba673c887cf
KennethEllebring <kenneth.ellebring@yh.nackademin.se>
2023-10-19 13:16:36 +0200

    fix: changed dollar to kr

--- app/components/cart/cart.tsx
+++ app/components/cart/cart.tsx
@@ -43,7 +43,7 @@ export default function Cart() {
           ))}
         <div className="flex justify-between items-center mt-4">
           <span className="text-lg font-bold">Total:</span>
-          <span className="text-xl font-bold">${total.toFixed(2)}</span>
+          <span className="text-xl font-bold">{total.toFixed(2)} kr</span>
         </div>
       </div>
     </>
