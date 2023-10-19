acb9d18b15df3a3b6f34c13f4f57e02807053e3d
KennethEllebring <kenneth.ellebring@yh.nackademin.se>
2023-10-19 13:10:15 +0200

    fix: removed som not needed elements

--- app/category/[categoryId]/page.tsx
+++ app/category/[categoryId]/page.tsx
@@ -1,10 +1,7 @@
 import Image from 'next/image';
 import prisma from '../../utils/connectdb';
 import Modal from '../../components/productmodal'
-
-interface param{
-  param: param
-}
+import AddToCart from '../../components/addToCart';
 
 
 export default async function Category({params}:{params:{categoryId: string};
@@ -38,21 +35,20 @@ export default async function Category({params}:{params:{categoryId: string};
               alt={product.description}
               className="mt-6 mb-4 hover:scale-110 transition duration-500 cursor-pointer"
             />
-             
 
             <h2 className="text-xl h-16 text-black mx-4 ">{product.name}</h2>
-            
+
             <p className="text-2xl h-8 text-red-600 font-black min-w-fit">
               {product.price?.toNumber()} kr
             </p>
             <small className="text-slate-400">
               {product.weight.toString()}g
             </small>
-          
-            <button className="mt-4 mb-4 align-bottom w-28 bg-transparent hover:bg-purple-800 text-purple-800 font-semibold hover:text-white py-2 px-4 border border-purple-500 hover:border-transparent rounded">
-              {/* Pass imageUrl and description as props to Modal */}
-              <Modal product={product}/>
-            </button>
+
+            <div className="flex justify-between w-52">
+              <Modal product={product} />
+              <AddToCart product={product} />
+            </div>
           </div>
         </div>
       ))}
