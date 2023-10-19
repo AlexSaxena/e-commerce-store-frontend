9a78edc7f85800d98644cf4fc7d1938d2cf62350
AlexSaxena <saxenaalex@gmail.com>
2023-10-19 18:27:33 +0200

    feat: updates from with correct var data & state
    Co-authored-by: Emil Sundén <emilsunjobb@gmail.com>

--- app/components/HandleSubmitButton.tsx
+++ app/components/HandleSubmitButton.tsx
@@ -6,16 +6,21 @@ import React, { useState } from 'react';
 // let storeId = '4d1875a7-1a5a-42d1-a9c1-ffa1b78bba20'; //Temporary Store ID
 
 let emilstore = '1b9f737b-8ed7-4b20-b3e7-d45dcd91eebe';
-let alexStore = '15fd1e68-0410-41e4-80f9-19c4f1128309';
+let alexStore = '1e683312-8daf-4a02-8577-842c545fd49a';
 
 const HandleSubmitButton: React.FC = () => {
   const [name, setName] = useState('');
   const [phone, setPhone] = useState('');
   const [address, setAddress] = useState('');
 
+  console.log('type of name -> ', typeof name);
+  console.log('type of address -> ', typeof address);
+  console.log('type of phone -> ', typeof phone);
+
   const cart = useCartStore((state) => state.cart);
   const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
-    console.log('Cart -> ', cart);
+    // console.log('Cart -> ', cart);
+    e.preventDefault();
 
     const myOrder: object[] = [];
 
@@ -43,16 +48,16 @@ const HandleSubmitButton: React.FC = () => {
       address: address,
     };
 
-    console.log('DATA --->', data);
+    // console.log('DATA --->', data);
 
-    console.log(
-      'Data orderItems',
-      data.orderItems.map((i) => console.log('i-> ', i)),
-    );
+    // console.log(
+    //   'Data orderItems',
+    //   data.orderItems.map((i) => console.log('i-> ', i)),
+    // );
 
     try {
       const response = await fetch(
-        `http://localhost:3000/api/${alexStore}/orders/addorder`, // CHECK LOCALHOST NR
+        `http://localhost:3001/api/${alexStore}/orders/addorder`, // CHECK LOCALHOST NR
         {
           mode: 'no-cors',
           method: 'POST',
@@ -62,14 +67,16 @@ const HandleSubmitButton: React.FC = () => {
           body: JSON.stringify(data),
         },
       );
+      console.log('response -> ', response);
 
       if (response.status === 200) {
         const data = await response.json();
-
+        console.log('200 Order Skickad');
+        console.log('response 200 -> ', response);
         return data;
       } else {
         localStorage.clear();
-
+        console.log('response inte 200-> ', response);
         throw new Error('Something went wrong with data retrieval!');
       }
     } catch (error) {
@@ -78,49 +85,51 @@ const HandleSubmitButton: React.FC = () => {
   };
 
   return (
-    <form action="" className="flex flex-col gap-1">
-      <div className="flex flex-col gap-1">
-        <div>
-          <label htmlFor="customerName">Namn: </label>
-          <input
-            type="text"
-            name="customerName"
-            id="customerName"
-            placeholder="Mitt Namn"
-            onChange={(e) => setName(e.target.value)}
-          />
+    <div>
+      <form action="" className="flex flex-col gap-1">
+        <div className="flex flex-col gap-1">
+          <div>
+            <label htmlFor="customerName">Namn: </label>
+            <input
+              type="text"
+              name="customerName"
+              id="customerName"
+              placeholder="Mitt Namn"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="customerTel">Tel Nr: </label>
+            <input
+              type="tel"
+              name="customerTel"
+              id="customerTel"
+              placeholder="Mitt Nr.."
+              onChange={(e) => setPhone(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="customerAdress">Adress: </label>
+            <input
+              type="text"
+              name="customerAdress"
+              id="customerAdress"
+              placeholder="Min Adress"
+              onChange={(e) => setAddress(e.target.value)}
+            />
+          </div>
         </div>
-        <div>
-          <label htmlFor="customerTel">Tel Nr: </label>
-          <input
-            type="tel"
-            name="customerTel"
-            id="customerTel"
-            placeholder="Mitt Nr.."
-            onChange={(e) => setPhone(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="customerAdress">Adress: </label>
-          <input
-            type="text"
-            name="customerAdress"
-            id="customerAdress"
-            placeholder="Min Adress"
-            onChange={(e) => setAddress(e.target.value)}
-          />
-        </div>
-      </div>
 
-      <button
-        type="submit"
-        // disabled={true}
-        className="mt-4 mb-4 align-bottom w-28 bg-transparent hover:bg-purple-800 text-purple-800 font-semibold hover:text-white py-2 px-4 border border-purple-500 hover:border-transparent rounded"
-        onClick={handleSubmit}
-      >
-        Lägg Order
-      </button>
-    </form>
+        <button
+          type="submit"
+          // disabled={true}
+          className="mt-4 mb-4 align-bottom w-28 bg-transparent hover:bg-purple-800 text-purple-800 font-semibold hover:text-white py-2 px-4 border border-purple-500 hover:border-transparent rounded"
+          onClick={handleSubmit}
+        >
+          Lägg Order
+        </button>
+      </form>
+    </div>
   );
 };
 
