c7639967c847662a603d14daea6d1c092405f954
AlexSaxena <saxenaalex@gmail.com>
2023-10-21 19:26:47 +0200

    fix: typo in svg variables

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
@@ -6,9 +6,7 @@ import logo from '../../public/assets/logos/logo-hakim-01.png';
 import { useCartStore } from './store/cartStore';
 import { ShoppingCart } from 'lucide-react';
 
-
 export default function Navbar() {
-
   const cart = useCartStore((state) => state.cart);
 
   return (
@@ -24,11 +22,26 @@ export default function Navbar() {
           />
         </Link>
         <div className="mr-16 pt-4 static">
-          <Link href="/checkout" className='flex items-center'>
-          <p className='text-2xl'>Kundvagn</p> 
-          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-shopping-cart"><circle cx="8" cy="21" r="1"/><circle cx="19" cy="21" r="1"/><path d="M2.05 2.05h2l2.66 12.42a2 2 0 0 0 2 1.58h9.78a2 2 0 0 0 1.95-1.57l1.65-7.43H5.12"/></svg>
+          <Link href="/checkout" className="flex items-center">
+            <p className="text-2xl">Kundvagn</p>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="32"
+              height="32"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="lucide lucide-shopping-cart"
+            >
+              <circle cx="8" cy="21" r="1" />
+              <circle cx="19" cy="21" r="1" />
+              <path d="M2.05 2.05h2l2.66 12.42a2 2 0 0 0 2 1.58h9.78a2 2 0 0 0 1.95-1.57l1.65-7.43H5.12" />
+            </svg>
           </Link>
-          <span className='bg-red-700 text-white text-xs px-1 rounded-full relative bottom-10 left-32'>
+          <span className="bg-red-700 text-white text-xs px-1 rounded-full relative bottom-10 left-32">
             {cart.length}
           </span>
         </div>
