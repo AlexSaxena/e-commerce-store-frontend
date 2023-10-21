7c390343b54185d8bf0add711685ce2403457727
KennethEllebring <kenneth.ellebring@yh.nackademin.se>
2023-10-21 17:58:44 +0200

    fix: changed to fetch instead of prismaconnect and decimal fixes

--- app/components/Product_list.tsx
+++ app/components/Product_list.tsx
@@ -1,14 +1,47 @@
-import Image from 'next/image';
-import prisma from '../utils/connectdb';
-import Modal from './productmodal'; // Make sure to import the correct Modal component
-import AddToCart from './addToCart';
+'use client'
 
-export default async function Product_list() {
-  const products = await prisma.product.findMany({
-    where: {
-      storeId: '4d1875a7-1a5a-42d1-a9c1-ffa1b78bba20',
-    },
-  });
+import Image from 'next/image';
+import Modal from './productmodal'; // Make sure to import the correct Modal component
+import { Product } from '@prisma/client';
+import AddToCart from './addToCart';
+import { useEffect, useState } from 'react';
+
+
+
+export default function Product_list() {
+  const STORE_ID = "4d1875a7-1a5a-42d1-a9c1-ffa1b78bba20"; // storeID from Hakim_livs
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    async function fetchProducts() {
+      try {
+        const response = await fetch(
+          `https://e-commerce-store-dashboard.vercel.app/api/${STORE_ID}/products`,
+          // `http://localhost:3001/api/${STORE_ID}/products`, // CHECK HOST NR
+          {
+            method: 'GET',
+          },
+        );
+        if (response.ok) {
+          const productsData = await response.json();
+          setProducts(productsData);
+        } else {
+          console.error('Failed to fetch Products.');
+        }
+      } catch (error) {
+        console.error('Error fetching Products:', error);
+      }
+    }
+    fetchProducts();
+  }, [products]);
+
+  
+  ///// OLD
+  // const products = await prisma.product.findMany({
+  //   where: {
+  //     storeId: '4d1875a7-1a5a-42d1-a9c1-ffa1b78bba20',
+  //   },
+  // });
 
   return (
     <div className="flex flex-wrap justify-center gap-8 text-center w-5/6">
@@ -29,10 +62,10 @@ export default async function Product_list() {
             <h2 className="text-xl h-16 text-black mx-4 ">{product.name}</h2>
 
             <p className="text-2xl h-8 text-red-600 font-black min-w-fit">
-              {product.price?.toFixed(2)} kr
+              {Number(product.price).toFixed(2)} kr
             </p>
             <small className="text-slate-400">
-              {product.weight.toString()}g
+              {product.weight} g
             </small>
 
             <div className="flex justify-between w-52">
--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -1,18 +1,40 @@
+'use client'
 import Link from 'next/link';
 import React from 'react';
-import prisma from '../utils/connectdb';
+import { Category } from '@prisma/client';
+import { useEffect, useState } from 'react';
 
-export default async function Sidebar() {
-  const categorys = await prisma.category.findMany({
-    where: {
-      storeId: '4d1875a7-1a5a-42d1-a9c1-ffa1b78bba20',
-    },
-  });
+export default function Sidebar() {
+
+  const STORE_ID = "4d1875a7-1a5a-42d1-a9c1-ffa1b78bba20"; // storeID from Hakim_livs
+  const [categories, setCategories] = useState<Category[]>([]);
+
+  useEffect(() => {
+    async function fetchCategories() {
+      try {
+        const response = await fetch(
+          `https://e-commerce-store-dashboard.vercel.app/api/${STORE_ID}/categories`,
+          {
+            method: 'GET',
+          },
+        );
+        if (response.ok) {
+          const categoriesData = await response.json();
+          setCategories(categoriesData);
+        } else {
+          console.error('Failed to fetch Categories.');
+        }
+      } catch (error) {
+        console.error('Error fetching Categories:', error);
+      }
+    }
+    fetchCategories();
+  }, [categories]);
 
   return (
     <aside role="Sidebar" className="	flex text-slate-800 text-2xl font-normal ">
       <ul className=" flex flex-col space-y-8 mt-4">
-        {categorys.map((category) => (
+        {categories.map((category) => (
           <Link
             key={category.id}
             href={`/category/${category.id}`}
