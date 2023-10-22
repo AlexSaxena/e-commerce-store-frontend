44a975100fd931a825aceb36f77342620c48e9e4
EmilSunden <emilsunjobb@gmail.com>
2023-10-22 19:45:45 +0200

    fix: deleted unnecessary line

--- app/page.tsx
+++ app/page.tsx
@@ -7,15 +7,13 @@ import 'react-toastify/dist/ReactToastify.css';
 import Billboard from './components/billboard';
 import getBillboard from './actions/get-billboard';
 
-export const revalidate = 0;
-
 export default async function Home() {
   const billboard = await getBillboard("e8e3cf67-a3f5-4010-85f8-bad5ab931eb7");
   return (
     <main>
       <div>
-      <Billboard data={billboard}/>
-      <Product_list />
+        <Billboard data={billboard}/>
+        <Product_list />
       </div>
     </main>
   );
