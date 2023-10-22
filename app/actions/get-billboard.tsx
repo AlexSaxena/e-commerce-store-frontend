b7c58012b5aeabd51f8c20ec2de414ba38f10030
EmilSunden <emilsunjobb@gmail.com>
2023-10-22 23:57:00 +0200

    fix: deleted use of env

--- app/actions/get-billboard.tsx
+++ app/actions/get-billboard.tsx
@@ -3,7 +3,7 @@ import { Billboard } from "@/types";
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/billboards`
 
 const getBillboard = async (id: string): Promise<Billboard> => {
-    const res = await fetch(`${URL}/${id}`)
+    const res = await fetch(`https://e-commerce-store-dashboard-git-billboard-jtts-projects.vercel.app/api/4d1875a7-1a5a-42d1-a9c1-ffa1b78bba20/billboards/${id}`)
     return res.json()
 };
 
